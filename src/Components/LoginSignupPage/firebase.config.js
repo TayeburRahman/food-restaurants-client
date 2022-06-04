bddf4aea28c2a36470412fa93ae20433c16e62a4
TayeburRahman <tayebrayhan101@gmail.com>
2022-06-05 00:26:19 +0600

    update

--- src/Components/LoginSignupPage/firebase.config.js
+++ src/Components/LoginSignupPage/firebase.config.js
@@ -1,12 +1,12 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCWZCSFBM4a0qh7F4Fsv7i0K5BPeJjvznY",
-  authDomain: "bingo-restaurant-fbde0.firebaseapp.com",
-  projectId: "bingo-restaurant-fbde0",
-  storageBucket: "bingo-restaurant-fbde0.appspot.com",
-  messagingSenderId: "808973542200",
-  appId: "1:808973542200:web:a9b4dcdb2461918a6a17cd",
-  measurementId: "G-SDVTNDZLHN"
+  apiKey: "AIzaSyC8avPS8jWzekuHZGh4NujFvpxzsYWYCKA",
+  authDomain: "food-factory-1dc80.firebaseapp.com",
+  projectId: "food-factory-1dc80",
+  storageBucket: "food-factory-1dc80.appspot.com",
+  messagingSenderId: "136934954007",
+  appId: "1:136934954007:web:52b6d08f328139d6e8f852",
+  measurementId: "G-L2F2KXGTZR"
 };
 export default firebaseConfig;
  
